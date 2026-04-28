b517fdd1a32d7a9cf8e044150c85f84236813c7d
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-04-28 13:47:05 +0600

    Creating Models for list of fields

--- djangoproject/app/models.py
+++ djangoproject/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class application(models.Model):
+    CHA_TYPE_CHOICE =[
+        ('RN','Rong Cha'),
+        ('DC','Dudh Cha'),
+        ('MC','Masala Cha'),
+        ('LC','Lebu Cha'),
+        ('AC','Ada Cha'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/')
+    date = models.DateTimeField(default=timezone.now)
+    chaType = models.CharField(max_length=2,choices=CHA_TYPE_CHOICE)
