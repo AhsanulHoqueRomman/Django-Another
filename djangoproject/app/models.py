09a8eb5211e1c9c3ed7cfbc3399a539e648f7164
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-04-29 21:57:05 +0600

    adding price field to the database and adding tailwind to style all of it

--- djangoproject/app/models.py
+++ djangoproject/app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class application(models.Model):
@@ -19,3 +21,50 @@ class application(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+##One To Many Relationship model:
+
+class chaReview(models.Model):
+
+    rating_choice= [
+        ('1','Very Low'),
+        ('2','Low'),
+        ('3','Medium'),
+        ('4','Good'),
+        ('5','Excellent')
+    ]
+
+    cha = models.ForeignKey(application, on_delete= models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(max_length=2, choices=rating_choice)
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.cha.name}'
+    
+
+
+#Many to Many Relationship:
+
+class store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    cha_varieties = models.ManyToManyField(application, related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+
+#One to One
+
+class chaCertificate(models.Model):
+    chai = models.OneToOneField(application, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_till = models.DateTimeField
+
+    def __str__(self):
+        return f'Certificate for {self.name.chai}'
