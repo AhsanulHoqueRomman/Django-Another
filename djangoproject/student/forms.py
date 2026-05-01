c2ba3627585662a512692ccc120be7fb3fed5e99
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-05-01 23:20:49 +0600

    adding css in forms and passing them to template

--- djangoproject/student/forms.py
+++ djangoproject/student/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from . import models
 from student.models import Profile
 
 
@@ -35,6 +36,10 @@ job_city_choice = (
 )
 
 class ProfileForm(forms.ModelForm):
+    gender = forms.ChoiceField(
+        choices=gender_choice,
+        widget=forms.RadioSelect
+    )
     class Meta:
         model = Profile
         fields = [
@@ -54,6 +59,12 @@ class ProfileForm(forms.ModelForm):
         }
 
         widgets ={
-            'name': forms.TextInput(attrs={'class': 'form-control'})
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'dob': forms.DateInput(attrs={'class': 'form-control', 'id':'datepicker','type':'date'}),
+            'locality': forms.TextInput(attrs={'class': 'form-control','placeholder':'Enter your area name' }),
+            'city': forms.TextInput(attrs={'class': 'form-control','placeholder':'Enter your city name' }),
+            'pin': forms.NumberInput(attrs={'class': 'form-control','placeholder':'Enter 6 digit PIN code' }),
+            'Zilla': forms.Select(attrs={'class':'form-select'})
+
         }
 
